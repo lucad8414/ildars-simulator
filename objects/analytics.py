3b48040b6bb0864ee1398ae174af455376585fa3
lucad8414 <luca.diringer@gmail.com>
2026-08-16 22:22:29 +0200

    Added comments to the analytics code, main function is still very messy, also still no random generating, but for now it works fine. TODOS: Refactor from Ray Tracing to Housholder for the reflection logic and create more analysis datasets and automate the analysis of those, to undermine the hypotheses/result better in the bachelor thesis.

--- objects/analytics.py
+++ objects/analytics.py
@@ -1,5 +1,8 @@
 def shredded(w: str) -> str:
     """
+    Takes a string, representing a reflection history and performs all the cancellation.
+    So if the same mirror is there back to back, then remove both, because it doesnt change
+    the image point.
     """
     if w == "": return w
 
@@ -7,33 +10,42 @@ def shredded(w: str) -> str:
         flag = True
         prev = ""
         for i, c in enumerate(w):
+            # same mirror back to back
             if prev == c:
+                # remove the mirrors
                 w = w[:i-1] + w[i+1:]
+                # can't end the loop here, because new cancelation could have been created.
                 flag = False
                 break
             prev = c
 
+        # if we went through the string and no cancellation appeared, then we can return the string.
         if flag:
             break
     return w
 
+# ----------------------------------------------------------------------------
 
 def matchings(p1: tuple[str, str], p2: tuple[str, str]) -> bool:
     """
-    Erkennt, ob p2 aus p1 durch Anfügen derselben Wand an beide Pfade entsteht.
-    Bsp: p1 = ('', '1') und p2 = ('2', '12') -> beide wurden rechts um '2' erweitert (oder links).
+    Checks whether p1 is reducable to p2 with some mirror sequence s.
     """
     Iw1, Iw2 = p1
     Iv1, Iv2 = p2
-    
+
+    # remove the I
     w1 = Iw1[1:]
     w2 = Iw2[1:]
     v1 = Iv1[1:]
     v2 = Iv2[1:]
 
+    # created the two possible mirror sequences with inversing w1, so we have
+    # w1 + w1^-1, then adds either v1 or v2, so the entire result is v1 or v2
     s1 = w1[::-1] + v1
     s2 = w1[::-1] + v2
 
+    # checks if this sequence also works for w2
+    # if anyone does, these are equal distance point pairs, if none does, then tey are not.
     if shredded(w2 + s1) == v2:
         return True
 
@@ -42,26 +54,44 @@ def matchings(p1: tuple[str, str], p2: tuple[str, str]) -> bool:
 
     return False
 
+# ----------------------------------------------------------------------------
 
 def split_dictionary_with_flags(data: dict):
+    """
+    Works entirely on filtered data. So demands a structure:
+    {
+    distance (float) : list of pairs with key's distance (list(tuple(string, string)))
+    }
 
+    Also wants the data to have filtered out all the length one lists, because they can't have
+    any group.
+    """
+
+    # alphabet for labeling in a group of 10 < size < 37
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    # Output initialisation
     clear = {}
     rest = {}
     logs = {}
 
+    # go through the data
     for key, value in data.items():
 
+        # make an empty flag entry for each point pair in the iteration.
         flags = [""] * len(value)
 
-        for i in range(len(value)):
+        # compare each point paire with each other:
+        for i in range(len(value)): 
             for j in range(len(value)):
 
                 if i == j: continue
 
+                # boolean, if they are reducable to each other, through some reflection sequence s
                 matches = matchings(value[i], value[j])
 
+                # add label, so we later now, that these two are in a group
                 if matches:
                     append_for_i = ""
                     append_for_j = ""
@@ -81,10 +111,12 @@ def split_dictionary_with_flags(data: dict):
         clear[key] = []
         rest[key] = []
 
+        # format the falgs, filter out the doubles.
         ff = []
         for flag in flags:
             ff.append("".join(dict.fromkeys(flag)))
 
+        # split all the grouped once with those who are groupless:
         logs[key] = ff
         for ind, flag in enumerate(flags):
             if flag != "":
